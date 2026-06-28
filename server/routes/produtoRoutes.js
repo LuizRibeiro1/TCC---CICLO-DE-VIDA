fa482ce1511d7ea97e2d3b31076eec6dd2642c40
Favarato05 <matheusfavarato13@gmail.com>
2026-06-28 00:35:08 -0300

    Reapply commit vai

--- server/routes/produtoRoutes.js
+++ server/routes/produtoRoutes.js
@@ -9,11 +9,6 @@ const { verificarToken } = require("../middleware/authMiddleware.js")
 
 router.get("/cadastrar", verificarToken, produtoController.exibirCadastro)
 router.post("/cadastrar", verificarToken, produtoController.cadastrar)
-router.get("/:id/editar", verificarToken, produtoController.exibirEdicao)
-router.post("/:id/editar", verificarToken, produtoController.editar)
-router.get("/:id/movimentar", verificarToken, produtoController.exibirMovimentacao)
-router.post("/:id/movimentar", verificarToken, produtoController.movimentar)
-router.post("/:id/desativar", verificarToken, produtoController.desativar)
 
 router.get("/:id/editar", verificarToken, produtoController.exibirEditar)
 router.post("/:id/atualizar", verificarToken, produtoController.atualizar)
